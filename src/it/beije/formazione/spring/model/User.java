6d978bd22e397653e33c2b5ab7cacb6d10cc99f6
Padawan05 <Padawan05@192.168.1.86>
2019-05-29 12:50:41 +0200

    FLAWLESS FOR REAL

--- src/it/beije/formazione/spring/model/User.java
+++ src/it/beije/formazione/spring/model/User.java
@@ -1,32 +1,67 @@
 package it.beije.formazione.spring.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
-	private String firstName;
-	private String lastName;
-	private String personalEmail;
-	private String workEmail;
-	private String phone;
-	private String fiscalCode;
-	private boolean admin;
-	private String password;
 	
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "personal_email")
+	private String personalEmail;
+	
+	@Column(name = "work_email")
+	private String workEmail;
+	
+	@Column(name = "phone")
+	private String phone;
+	
+	@Column(name = "fiscal_code")
+	private String fiscalCode;
+	
+	@Column(name = "admin")
+	private boolean admin;
+	
+	@Column(name = "password")
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+	public void setFirstName(String nome) {
+		this.firstName = nome;
 	}
-	
 
 	public String getLastName() {
 		return lastName;
 	}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setLastName(String cognome) {
+		this.lastName = cognome;
 	}
-
 
 	public String getPersonalEmail() {
 		return personalEmail;
@@ -75,5 +110,7 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+
 
 }
