94be6646a24c18982ad5d89e0e49f53d1c138749
ivfly <ivo.mosca@privategriffe.com>
2019-06-06 15:42:28 +0200

    Service

---
+++ src/it/beije/erp/timesheet/service/UserService.java
@@ -0,0 +1,102 @@
+package it.beije.erp.timesheet.service;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Service;
+
+import it.beije.formazione.spring.model.User;
+import it.beije.jpa.JpaEntityManager;
+
+@Service
+public class UserService {
+
+	public void create(User user) {
+		EntityManagerFactory emfactory = JpaEntityManager.getInstance();
+
+		EntityManager entitymanager = emfactory.createEntityManager();
+		entitymanager.getTransaction().begin();
+
+		User utente = new User();
+		//	      utente.setId(141);
+		utente.setFirstName(user.getFirstName());
+		utente.setLastName(user.getLastName());
+		utente.setPersonalEmail(user.getPersonalEmail());
+		utente.setWorkEmail(user.getWorkEmail());
+		utente.setPhone(user.getPhone());
+		utente.setFiscalCode(user.getFirstName());
+		utente.setAdmin(0);
+		utente.setPassword(user.getPassword());
+
+		entitymanager.persist(utente);
+		entitymanager.getTransaction().commit();
+
+//		entitymanager.close( );
+//		emfactory.close( );
+	}
+
+	public static String trovaUtente(String firstName, String lastName) {
+
+		EntityManagerFactory emfactory = JpaEntityManager.getInstance();
+		EntityManager entitymanager = emfactory.createEntityManager();
+
+		String ricerca =" WHERE ";
+
+		//se null o vuoto non metto condizione WHERE
+		if((firstName  == null || firstName.length() == 0 )
+				&&( lastName  == null || lastName.length() == 0)) {
+			ricerca ="";
+
+		} else {
+
+			//Condizione WHERE
+			if(firstName.length() > 0 && lastName.length() > 0) {
+				ricerca += "a.first_name = '" +firstName + "'";
+				ricerca += " AND ";
+				ricerca += "a.last_name = '" +lastName + "'";
+			}else {
+				if(firstName.length() > 0)
+					ricerca += "a.first_name = '" +firstName + "'";
+				if(lastName.length() > 0)
+					ricerca += "a.last_name = '" +lastName + "'";
+			}
+
+		}
+		System.out.println("Sto cercando");
+		//OK
+		//		Query q = entitymanager.createNativeQuery("SELECT a.first_name, a.last_name FROM user a");
+		//		Query q = entitymanager.createNativeQuery("SELECT a.id, a.first_name, a.last_name FROM user a"
+		//				+ " WHERE a.first_name = " + "'"+ firstName +"'");
+
+		Query q = entitymanager.createNativeQuery("SELECT * FROM user a"
+				+ ricerca);
+		
+		List<Object[]> utenti = q.getResultList();
+
+		String trovati ="";
+//		System.out.println("Utenti trovati: ");
+		for (Object[] u : utenti) {
+//			System.out.println(
+//					"ID: " + u[0]  + " " +	           
+//							"Nome: "+ u[1] + " " +	  
+//							"Cognome: " +u[2] + " " +	
+//							"Codice Fiscale: " +u[6]			
+//					);
+			trovati += "<b>ID:</b> " + u[0]  + " " +	           
+					"<b>Nome:</b> "+ u[1] + " " +	  
+					"<b>Cognome:</b> " +u[2] + " " +	
+					"<b>Codice Fiscale:</b> " +u[6] + "<br><br>";
+//			System.out.println(trovati);
+
+		}
+		
+		if(trovati.length()==0)
+			trovati = "<b>Ops! Nessun utente trovato con questi parametri.<b>";
+
+		return trovati;
+	}
+
+}
