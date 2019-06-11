c46235ea531a98687acf83909cfe16a5b2444194
Padawan07 <Padawan07@DESKTOP-0GNJ6D6.lan>
2019-06-11 16:52:16 +0200

    update

--- src/it/beije/erp/timesheet/service/TimetableService.java
+++ src/it/beije/erp/timesheet/service/TimetableService.java
@@ -104,7 +104,7 @@ public class TimetableService {
 		}
 		
 	//RECUPERA UTENTE PER ID - da DATA a DATA
-//modifica casuale
+//modifica casuale2
 	public void updateRecord(int id, Date date,Timetable newTable) {
 		EntityManagerFactory emfactory = JpaEntityManager.getInstance();
 		EntityManager entitymanager = emfactory.createEntityManager();
