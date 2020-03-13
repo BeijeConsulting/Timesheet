8135420a415e9220678c294163ae15a575184120
Clark <clarke2897@gmail.com>
2020-03-13 14:17:38 +0100

    per connessione

--- src/it/beije/mgmt/tools/importuser/DatabaseConnection.java
+++ src/it/beije/mgmt/tools/importuser/DatabaseConnection.java
@@ -9,15 +9,14 @@ public class DatabaseConnection {
 	protected static Connection initializeDatabase() 
 		throws SQLException, ClassNotFoundException 
 	{ 
+	
+		String dbURL = "jdbc:mysql://localhost:3306/timesheet?serverTimezone=CET"; 
 		
-		String dbURL = "jdbc:mysql://beijedev.ccc2op6yesad.eu-west-1.rds.amazonaws.com:3306/"; 
-		
-		String dbName = "beijedb?serverTimezone=CET"; 
-		String dbUsername = "beije"; 
-		String dbPassword = "beije001"; 
+		String dbUsername = "root"; 
+		String dbPassword = "Beije14"; 
 
 		Class.forName("com.mysql.cj.jdbc.Driver");  
-		Connection con = DriverManager.getConnection(dbURL + dbName, dbUsername, dbPassword); 
+		Connection con = DriverManager.getConnection(dbURL, dbUsername, dbPassword); 
 		return con; 
 	} 
 } 
