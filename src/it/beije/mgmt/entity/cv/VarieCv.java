197cbfe862c9cae44af10280e6c812a1190a3bb8
Padawan04 <Padawan04@DESKTOP-LV5RKMD>
2020-03-17 14:59:17 +0100

    aggiunta entity

--- src/it/beije/mgmt/entity/cv/VarieCv.java
+++ src/it/beije/mgmt/entity/cv/VarieCv.java
@@ -1,10 +1,14 @@
 package it.beije.mgmt.entity.cv;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "varie_cv")
 public class VarieCv {
 	
 	@Id
