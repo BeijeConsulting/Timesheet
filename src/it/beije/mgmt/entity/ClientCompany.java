f9f45b3dbfe94bc579521d499bc2420191f57ec9
Padawan09 <Padawan09@DESKTOP-O65P8H4>
2020-03-19 15:37:01 +0100

    annotations

--- src/it/beije/mgmt/entity/ClientCompany.java
+++ src/it/beije/mgmt/entity/ClientCompany.java
@@ -16,15 +16,29 @@ import javax.persistence.Table;
 
 
 @Entity
-@Table(name = "client_company")
+@Table(name = "company")
 public class ClientCompany {	
-		
-	private static final long serialVersionUID = 4865903039190150223L;
 
 	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
+	
+	public ClientCompany() {
+		super();
+	}
+
+	public ClientCompany(ClientCompany client) {
+		super();
+		this.id = client.id;
+		this.firstName = client.firstName;
+		this.lastName = client.lastName;
+		this.email = client.email;
+		this.phone = client.phone;
+		this.secondaryEmail = client.secondaryEmail;
+		this.offices = client.offices;
+		this.relativeUser = client.relativeUser;
+	}
 
 	@Column(name = "first_name", nullable=false)
 	private String firstName;
@@ -44,6 +58,66 @@ public class ClientCompany {
 	@OneToMany(cascade=CascadeType.ALL)
 	@JoinColumn(name="id_client_company")
 	private List<Office> offices;
+	
+	@OneToMany(mappedBy = "company",
+			cascade = CascadeType.ALL,
+			orphanRemoval = true)
+	private List<UserHasClient> relativeUser;
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getSecondaryEmail() {
+		return secondaryEmail;
+	}
+
+	public void setSecondaryEmail(String secondaryEmail) {
+		this.secondaryEmail = secondaryEmail;
+	}
+
+	public List<Office> getOffices() {
+		return offices;
+	}
+
+	public void setOffices(List<Office> offices) {
+		this.offices = offices;
+	}
+
+	public List<UserHasClient> getRelativeUser() {
+		return relativeUser;
+	}
+
+	public void setRelativeUser(List<UserHasClient> relativeUser) {
+		this.relativeUser = relativeUser;
+	}
 	
 }
