256c368b2f91ae9d205ccaf34bf5b1ef7a38c157
Padawan11 <Padawan11@DESKTOP-MQ1AMB6.lan>
2020-03-19 12:01:23 +0100

    Delete CVRepository, Modifiche a WorkApiController, e CvService (Metodi Work)

--- src/it/beije/mgmt/repository/CVRepository.java
+++
@@ -1,9 +0,0 @@
-package it.beije.mgmt.repository;
-
-import java.util.List;
-
-import it.beije.mgmt.entity.cv.Work;
-
-public interface CVRepository {
-	List<Work> findByIdWork(Long idWork);
-}
--- src/it/beije/mgmt/service/CvService.java
+++ src/it/beije/mgmt/service/CvService.java
@@ -3,8 +3,6 @@ package it.beije.mgmt.service;
 import java.util.List;
 import org.springframework.transaction.annotation.Transactional;
 
-import it.beije.mgmt.entity.Address;
-import it.beije.mgmt.entity.User;
 import it.beije.mgmt.entity.cv.CV;
 import it.beije.mgmt.entity.cv.Certification;
 
@@ -14,13 +12,11 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import it.beije.mgmt.entity.cv.Formazione;
 import it.beije.mgmt.entity.cv.Work;
 import it.beije.mgmt.jpa.JpaEntityManager;
-import it.beije.mgmt.repository.CVRepository;
 
 @Service
 public class CvService {
@@ -128,55 +124,73 @@ public class CvService {
 
 
 	/**** WORK ****/
-	@Autowired
-	private CVRepository CvRepository;
+	// GET Work by User Id
+	@Transactional
+	public List<Work> getListWorkByUserId(Long idCv) {
+		List<Work> listWorks = new ArrayList<Work>();
 
-	public Work setWork(Long idWork, Work work) throws Exception {
 		EntityManager entityManager = Persistence.createEntityManagerFactory("timesheet").createEntityManager();
 		entityManager.getTransaction().begin();
-		Work w = entityManager.find(Work.class, idWork);
-		CV cv = new CV();
-		System.out.println("cv.getWorkList()?" 
-				+ (cv.getWorkList() != null ? cv.getWorkList().size() : "NULL"));
-		if (Objects.isNull(w.getIdWork())) {
-			w.setIdWork(idWork);
-		} else if (w.getIdWork().longValue() != idWork.longValue()) {
-			throw new Exception();
-		}	
+
+		listWorks = entityManager.createQuery("select c from Certification c where c.id_cv = " + idCv, Work.class).getResultList();
+
+		return listWorks;
+	}
+
+
+	// POST Work for user
+	@Transactional
+	public void insertNewWorkForUser(Long idCv, Work work) throws Exception {
+
+		EntityManager entityManager = Persistence.createEntityManagerFactory("timesheet").createEntityManager();
+		entityManager.getTransaction().begin();
+
+		CV cv = entityManager.find(CV.class, idCv);
+
+		if (Objects.isNull(work.getIdCV())) {
+			work.setIdWork(idCv);
+		} else {
+			if (work.getIdCV().longValue() != idCv.longValue()) {
+				throw new Exception();
+			}
+		}
+
 		List<Work> Works = cv.getWorkList();
+
+
 		Works.add(work);
 		cv.setWorkList(Works);
-		entityManager.persist(cv);
+
+		entityManager.persist(work);
 		entityManager.getTransaction().commit();
 		entityManager.close();
-		return work;
+
 	}
+
+	// PUT Work by IdCertification
 	@Transactional
-	public List<Work> getWork(Long idWork) {
-		List<Work> Works = CvRepository.findByIdWork(idWork);
-		System.out.println("addresses Size : " + Works.size());
-		return Works;
-	}
-	@Transactional
-	public Work updateWork(Long idWork, Work work) {
+	public void updateWorkById(Long id, Work work) {
 		EntityManagerFactory emfactory = JpaEntityManager.getInstance();
+
 		EntityManager entitymanager = emfactory.createEntityManager();
 		entitymanager.getTransaction().begin();
-		Work w = entitymanager.find(Work.class, idWork);
-		if (work.getTitle() != null) w.setTitle(work.getTitle());
-		if (work.getEmployment() != null) w.setEmployment(work.getEmployment());
-		if (work.getCompany() != null) w.setCompany(work.getCompany());
-		if (work.getLocation() != null) w.setLocation(work.getLocation());
-		if (work.getStartDate() != null) w.setStartDate(work.getStartDate());
-		if (work.getEndDate() != null) w.setEndDate(work.getEndDate());
-		if (work.getDescription() != null) w.setDescription(work.getDescription());
-		if (work.getTechnologies() != null) w.setTechnologies(work.getTechnologies());
+
+		Work updateWork = entitymanager.find(Work.class, id);
+
+		if (work.getTitle() != null) updateWork.setTitle(work.getTitle());
+		if (work.getEmployment() != null) updateWork.setEmployment(work.getEmployment());
+		if (work.getCompany() != null) updateWork.setCompany(work.getCompany());
+		if (work.getLocation() != null) updateWork.setLocation(work.getLocation());
+		if (work.getStartDate() != null) updateWork.setStartDate(work.getStartDate());
+		if (work.getEndDate() != null) updateWork.setEndDate(work.getEndDate());
+		if (work.getDescription() != null) updateWork.setDescription(work.getDescription());
+		if (work.getTechnologies() != null) updateWork.setTechnologies(work.getTechnologies());
 		entitymanager.persist(work);
 		entitymanager.getTransaction().commit();
 		entitymanager.close();
-		return work;
 	}
 
+		
 	/**** CERTIFICATION ****/
 	// GET Certification by User Id
 	@Transactional
