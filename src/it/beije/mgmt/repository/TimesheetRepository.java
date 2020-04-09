a609f4c1c192928f21395da8f11441173cd898ae
ivfly <iv0fly@gmail.com>
2020-04-09 11:36:35 +0200

    prime correzioni ai repo

--- src/it/beije/mgmt/repository/TimesheetRepository.java
+++ src/it/beije/mgmt/repository/TimesheetRepository.java
@@ -10,19 +10,20 @@ import it.beije.mgmt.entity.Timesheet;
 @Repository
 public interface TimesheetRepository extends JpaRepository<Timesheet, Long> {
 	
-	List<Timesheet> findByIdUtenteAndStartDate(int idUtente, Date startDate );
+	List<Timesheet> findByIdUserAndStartDate(Long idUser, Date startDate );
 	
-	List<Timesheet> findByIdUtenteAndStartDateGreaterThanEqual(int idUtente, Date startDate );
+	List<Timesheet> findByIdUserAndDateGreaterThanEqual(Long idUser, Date date );
 	
-	List<Timesheet> findByIdUtente(int idUtente);
+	List<Timesheet> findByIdUser(Long idUser);
 	
-	List<Timesheet> findByIdUtenteAndDateFromBetweenDateTo(int idUtente, Date dateFrom, Date dateTo);
+	List<Timesheet> findByIdUserAndDateFromBetweenDateTo(Long idUser, Date dateFrom, Date dateTo);
 	
-	List<Timesheet> findByDateFromBetweenDateTo(Date dateFrom, Date dateTo);
+	//List<Timesheet> findByDateBetweenDate(Date dateFrom, Date dateTo);
+	List<Timesheet> findByDateGreaterThanEqualAndDateLessThanEqual(Date dateFrom, Date dateTo);
 	
-	Timesheet deleteByIdUtenteAndDateFromAndType(int idUtente, Date dateFrom, String Type);
+	Timesheet deleteByIdUserAndDateFromAndType(Long idUser, Date dateFrom, String Type);
 	
-	List<Timesheet> deleteByIdUtenteAndDateFrom(int idUtente, Date dateFrom);
+	List<Timesheet> deleteByIdUserAndDateFrom(Long idUser, Date dateFrom);
 	
 	
 
